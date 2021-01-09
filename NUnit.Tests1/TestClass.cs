a867eaa2d760d40251754f1232d1ccf892d59c07
NouranAbdeen96 <nouran.m.abdeen@gmail.com>
2021-01-09 17:59:54 +0200

    Initial Project

---
+++ NUnit.Tests1/TestClass.cs
@@ -0,0 +1,33 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        IWebDriver webDriver;
+        [SetUp]
+        public void Initialize()
+        {
+            webDriver = new ChromeDriver();
+        }
+        [Test]
+        public void TestMethod()
+        {
+            webDriver.Url = "https://www.airbnb.com/";
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            webDriver.Close();
+            webDriver.Quit();
+        }
+    }
+}
