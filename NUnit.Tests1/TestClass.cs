6cb42b99df61e5f123971a3af8ecb635bc7754a3
NouranAbdeen96 <nouran.m.abdeen@gmail.com>
2021-01-10 21:17:30 +0200

    Finished checking that the chosen filters appear correclty

--- NUnit.Tests1/TestClass.cs
+++ NUnit.Tests1/TestClass.cs
@@ -15,11 +15,25 @@ namespace NUnit.Tests1
     [TestFixture]
     public class TestClass
     {
-        IWebDriver webDriver;
+        static IWebDriver webDriver;
         [SetUp]
         public void Initialize()
         {
             webDriver = new ChromeDriver();
+        }
+        public static IWebElement WaitUntilElementClickable(By elementLocator, int timeout = 10)
+        {
+            //Source of code: https://stackoverflow.com/questions/43203243/how-to-get-webdriver-to-wait-for-page-to-load-c-selenium-project
+            try
+            {
+                var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementToBeClickable(elementLocator));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
+                throw;
+            }
         }
         public void SearchActions()
         {
@@ -65,17 +79,16 @@ namespace NUnit.Tests1
             }
             catch (Exception e)
             {
-                Assert.Fail("Exception Thrown when entering the location. Error Message: "+e.Message);
+                Assert.Fail("Exception Thrown when entering the location. Error Message: " + e.Message);
             }
 
             //Check-in and Check-out:
             try
             {
-                //Get the DIV that contains the Check in and out dates and click on it to open the calender
                 checkInOutDiv = webDriver.FindElement(By.ClassName("_j8gg2a"));
                 checkInInput = checkInOutDiv.FindElement(By.ClassName("_1akb2mdw"));
                 checkInInput.Click();
-
+                //Get the DIV that contains the Check in and out dates and click on it to open the calender
                 //Choose Check-in and Check-out dates
                 chosenCheckInDate = webDriver.FindElement(By.XPath(@"/html/body/div[4]/div/div/div/div[1]/div[1]/div/header/div/div[2]/div[2]/div/div/div/form/div/div/div[3]/div[4]/section/div/div/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[3]/td[6]"));
                 chosenCheckOutDate = webDriver.FindElement(By.XPath(@"/html/body/div[4]/div/div/div/div[1]/div[1]/div/header/div/div[2]/div[2]/div/div/div/form/div/div/div[3]/div[4]/section/div/div/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[4]/td[7]"));
@@ -120,10 +133,29 @@ namespace NUnit.Tests1
         public void TestCase1()
         {
             SearchActions();
+            //Waiting for a web element on the page to be clickable to be sure that the page is loaded
+            WaitUntilElementClickable(By.ClassName("_1usxwsg6"));
+
+            ////Getting the elements by class name: _1g5ss3l where the first three elements are for the applied filters
+            List<IWebElement> chosenFilters = new List<IWebElement>(webDriver.FindElements(By.ClassName("_1g5ss3l")));
+            if (chosenFilters[0].Text != "Rome")
+            {
+                Assert.Fail("The location filter is incorrect with value: " + chosenFilters[0].Text);
+            }
+            if (chosenFilters[1].Text != "Jan 15 – 23")
+            {
+                Assert.Fail("The Date filter is incorrect with value: " + chosenFilters[1].Text);
+            }
+            if (chosenFilters[2].Text != "3 guests")
+            {
+                Assert.Fail("The Number of guests filter is incorrect with value: " + chosenFilters[2].Text);
+            }
+
         }
         [TearDown]
         public void EndTest()
         {
+            //I had an issue with chromedriver.exe so I put close+quit both which helped in addition to killing the task from cmd if it keeps running in the background 
             webDriver.Close();
             webDriver.Quit();
         }
